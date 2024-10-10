878adf35c6c28b950a1c5fd0ca6b0e98fc7f4939
7e-sky <webmaster@7e-sky.com>
2024-10-10 15:15:11 +0100

    Import

--- src/app/main/acheteur/acheteurConfigs.js
+++ src/app/main/acheteur/acheteurConfigs.js
@@ -3,6 +3,7 @@ import { BlackListesAppConfig } from "./black_liste/BlackListesAppConfig";
 import { ProfileConfigs } from "./profile/ProfileConfigs";
 import { DashboardAppConfig } from "./dashboard/DashboardAppConfig";
 //import AcheteursPage from './AcheteursPage';
+import { ImportConfigs } from "./import/ImportConfigs";
 import { TeamsAppConfig } from "./Directeur_achat/TeamsAppConfig";
 import { childsConfigs } from "./childs/childsConfigs";
 import { SuggestionsAppConfig } from "./Demande_suggetion/SuggestionsAppConfig";
@@ -13,6 +14,7 @@ export const acheteurConfigs = [
     ProfileConfigs,
     TeamsAppConfig,
     childsConfigs,
+    ImportConfigs,
     SuggestionsAppConfig,
     FacturationAppConfig,
    // AcheteursPage,
